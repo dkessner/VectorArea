e6dd4e28db1e4b1441a4978b59c407708b8d0283
Darren Kessner <Darren.Kessner@marlborough.org>
2020-04-18 07:25:07 -0700

    saving code from some video tutorials

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,7 +9,7 @@
 void ofApp::setup()
 {
     arrow.setMode(OF_PRIMITIVE_LINE_STRIP);
-    arrow.addVertex(ofPoint(50, 50));
+    arrow.addVertex(glm::vec3(50, 50, 0));
     arrow.addVertex(ofPoint(100, 100));
 
     arrow.addColor(ofColor::white);
@@ -23,6 +23,35 @@ void ofApp::update()
 }
 
 
+
+namespace {
+
+void tutorial2D()
+{
+    ofNoFill();
+    ofDrawRectangle(64, 64, 64, 64);
+    ofDrawCircle(192, 96, 32);
+    ofDrawLine(256, 64, 320, 128);
+    ofDrawTriangle(352, 128, 384, 64, 416, 128);
+    ofDrawEllipse(512, 96, 128, 64);
+    ofDrawRectRounded(608, 64, 64, 64, 9);
+    ofDrawBezier(704, 64, 736, 128, 768, 160, 800, 64);
+}
+
+void tutorial3D()
+{
+    ofNoFill();
+    ofDrawBox(64, 256, 0, 64, 64, 64);
+    ofDrawCone(160, 256, 0, 32, 128);
+    ofDrawCylinder(256, 256, 32, 128);
+    ofDrawIcoSphere(352, 256, 0, 32);
+    ofDrawPlane(448, 256, 64, 64);
+    ofDrawSphere(544, 256, 32);
+}
+
+} // namespace
+
+
 void ofApp::draw()
 {
     ofBackground(0);
@@ -30,7 +59,12 @@ void ofApp::draw()
     ofDrawBitmapString("Hello, world!", 50, 50);
 
     arrow.draw();
+
+    //tutorial2D();
+    tutorial3D();
 }
+
+
 
 
 void ofApp::keyPressed(int key){}
